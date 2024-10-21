531073e0654e7572f4b5700d461cfb6edf2a74a9
JOHNROSSDOHERTY-STUD <g00423043@atu.ie>
2024-10-21 10:06:00 +0100

    j

--- src/main/java/ie/atu/studentapp/Main.java
+++ src/main/java/ie/atu/studentapp/Main.java
@@ -2,30 +2,33 @@ package ie.atu.studentapp;
 
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Please enter a number between 0-9 : ");
-        Scanner myscan = new Scanner(System.in);
 
-        String input = myscan.nextLine();
 
-        try {
+
+
+        try (Scanner myscan = new Scanner(System.in);){
+
+            String input = myscan.nextLine();
+
             int myNum = Integer.parseInt(input);
 
-            if (myNum >= 0 && myNum <= 9) {
-                System.out.println("You entered: " + myNum);
-            } else {
-                System.out.println("Not a valid number");
+            while (myNum > 0 || myNum < 9) {
+
+                try {
+
+                    if (myNum >= 0 && myNum <= 9) {
+                        System.out.println("You entered: " + myNum);
+                    } else {
+                        System.out.println("Not a valid number");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid input. Enter number between 0-9: ");
+                }
             }
-        }
-        catch(NumberFormatException e){
-            System.out.println("Invalid input. Enter number between 0-9: " );
-            String input1 = myscan.nextLine();
-        }
-        finally{
-            myscan.close();
+
         }
     }
 }
